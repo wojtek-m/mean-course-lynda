73f0b2fa01c3d9f367fda945630ae5ca516af372
Wojtek Murawski <wojciech.murawski@gmail.com>
2015-09-10 20:44:46 +0000

    assets + few routes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.get('/create', function(req, res, next) {
+    var vm = {
+        title: 'Create an account'
+    };
+    res.render('users/create', vm);
+});
+
+router.post('/create', function(req, res, next) {
+    var somethingGoesWrong = false;
+    if (somethingGoesWrong) {
+        var vm = {
+            title: 'Create an account',
+            input: req.body,
+            error: 'Something went wrong'
+        };
+        delete vm.input.password;
+        return res.render('users/create', vm);
+    }
+    res.redirect('/orders');
+});
+
+
+
 module.exports = router;
